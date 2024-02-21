9343a6eb90fa2864e1d7e5cba43577345edfae4d
Shamit Verma <github@shamit.in>
2024-02-21 19:38:30 +0530

    Initial SpringBoot project

---
+++ WebApp/src/main/java/in/shamit/nlp/wordvec/WebApp/WordVectorDemo.java
@@ -0,0 +1,13 @@
+package in.shamit.nlp.wordvec.WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordVectorDemo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordVectorDemo.class, args);
+	}
+
+}
---
+++ WebApp/src/test/java/in/shamit/nlp/wordvec/WebApp/WordVectorDemoTests.java
@@ -0,0 +1,13 @@
+package in.shamit.nlp.wordvec.WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordVectorDemoTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
