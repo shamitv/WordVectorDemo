22ef2e11a2105607eb38d5b07379cbb2674a760f
Shamit Verma <github@shamit.in>
2024-02-21 21:29:12 +0530

    Added Swagger to troubleshoot URI

--- WebApp/src/main/java/in/shamit/nlp/wordvec/demo/Config.java
+++ WebApp/src/main/java/in/shamit/nlp/wordvec/demo/Config.java
@@ -4,4 +4,6 @@ public class Config {
 	public static String VECTOR_FILE_GLOVE_COMMON_CRAWL_CASED="K:/nlp/word2vec/glove/glove.840B.300d.txt";
 	public static String VECTOR_FILE_SEC_EDGAR="K:/nlp/sec/edgar/sec_edgar~/vectors.vec";
 	public static String VECTOR_FILE_GLOVE_WIKI_GIGAWORD="K:/nlp/word2vec/glove/glove.840B.300d.txt";
+
+	public static String VECTOR_FILE_FASTTEXT_COMMON_CRAWL="K:/nlp/word2vec/glove/glove.840B.300d.txt";
 }
--- WebApp/src/main/java/in/shamit/nlp/wordvec/demo/services/PingService.java
+++ WebApp/src/main/java/in/shamit/nlp/wordvec/demo/services/PingService.java
@@ -3,11 +3,12 @@ package in.shamit.nlp.wordvec.demo.services;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service( "pingService" )
+@RestController
 public class PingService {
-    @GetMapping("ping/")
-    public String doPing(@RequestParam String ping){
-    	return "Pong "+ping;
+    @GetMapping("/api/ping/")
+    public String doPing(){
+    	return "Pong ";
     }
 }
